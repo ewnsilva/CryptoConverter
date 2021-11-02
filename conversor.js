5898b846596a1c402b8e3132ca5e468f49b2e417
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2021-11-02 15:32:44 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,6 +1,6 @@
-var brl = 1.0
-var btc = 349767.80
+const btc = 349767.80
 var inputMoeda = prompt("Insira o valor a ser convertido:")
 inputMoeda = parseFloat(inputMoeda)
+
 var valorConvertido = inputMoeda / btc
 console.log("Valor convertido:" + valorConvertido)
