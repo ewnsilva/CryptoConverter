5d9229d012f2e4c43bf7ed1ae3d42bb42a311e3b
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2021-12-01 23:33:32 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,13 +1,18 @@
-window.onload = cryptoConverter()
-    cliqueBotao();
+document.getElementById("converter").onclick = valorConvertido;
 
-const btc = 364148.54;
+function valorConvertido(){
+    
+    var inputMoeda = document.getElementById("inputMoeda").value;
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    const btc = 364148.54;
 
-var inputMoeda = document.getElementById("inputMoeda").innerHTML;
-    inputMoeda = parseFloat(inputMoeda);
-
-function obtendoResultado ()
-{
-    var resultado = inputMoeda / btc;
-    document.getElementById("resultado").innerHTML
-}
+    if(isNaN(inputMoeda)){
+        window.alert("Somente números.") 
+        }else if(inputMoeda === ''){
+        window.alert("Insira o valor a ser convertido.")
+        }
+    inputMoeda = parseFloat(inputMoeda) / btc;
+    resultado.innerHTML = inputMoeda + " BTC"
+    return
+    }
